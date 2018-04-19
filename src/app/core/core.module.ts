03d51296e42955569a69a0af379395ba37582e17
Minghao <minghao.wang@dbjtech.com>
2018-04-19 17:34:56 +0800

    feat: init the UI page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,26 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 // import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module'
+import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component'
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: []
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
 })
 /*
-*  如何实现让coreModule只加载一次
+*  如何实现让coreModule只加载一次,尽量让coreModule中的东西都只加载一次
 *  要避免注入时报错陷入循环，要导入SkipSelf装饰器
 *  要避免初次注入时找不到依赖，要注入Optional装饰器，说明依赖可选
 * */
