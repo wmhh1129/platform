6440e273237ce6a30b5bdf747253008c51ae7953
Minghao <446042310@qq.com>
2018-04-20 16:26:15 +0800

    feat: use angular-material-design to init the header,footer,sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdSidenavModule } from '@angular/material'
 import { CoreModule } from './core/core.module'
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MdSidenavModule,
     CoreModule
   ],
   providers: [],
